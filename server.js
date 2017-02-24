24c38f83f87352706a6187a63ed26582358b3273
Bence Hornyak <sir.bence15@gmail.com>
2017-02-24 16:55:25 +0000

    finished fcc headerparser

--- server.js
+++ server.js
@@ -5,17 +5,42 @@ const router = express.Router();
 
 
 router.get('/', function(req, res){
- var osText = req.headers['user-agent'];
+ const osText = req.headers['user-agent'];
  var langText = req.headers['accept-language'];
  const ip = req.headers['x-forwarded-for'];
  
+ var os = getTextBetween(osText, '(', ')');
+ 
+ var lang = langText.split(',')[0];
+ 
  res.json( { 
-  OS: osText,
-  language: langText,
+  OS: os, 
+  language: lang,
   ip: ip
  } );
  
 });
+
+function getTextBetween(text, char1, char2) {
+ var result = '';
+ var flag = false;
+ 
+ for (var i = 0; i<text.length; i++) {
+  if (text[i-1] === char1) {
+   flag = true;
+  }
+  
+  if (flag) {
+   if (text[i] === char2) {
+    break;
+   } else {
+   result+=text[i];
+   }
+  }
+ }
+
+ return result;
+}
 
 // all of our routes will be prefixed with /api
 app.use('/api', router);
